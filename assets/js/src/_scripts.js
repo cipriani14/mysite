d1a728e1ba542fc63d36b36ba03e7be57e853755
Justin Peacock <1271504-justinpeacock@users.noreply.gitlab.com>
2022-11-03 18:17:42 -0400

    updated dependencies

--- assets/js/src/_scripts.js
+++ assets/js/src/_scripts.js
@@ -4,7 +4,7 @@ new TypeIt('#type', {
   .type('I\'m Jason Cipriani')
   .pause(300)
   .options({speed: 45})
-  .type(', a writer for yadda yadda yadda.<br><br>', { delay: 400})
+  .type(', a writer for ZDNET, IGN, CNN Underscored, The Arena Group, InsideHook, and Tom\'s Guide.<br><br>', { delay: 400})
   .pause(200)
-  .type('Find me on <a href="#">twitter</a>, <a href="#">instagram</a>, and <s style="color: #c6c5c2;">facebook</s>.', { delay: 400})
+  .type('Find me on <a href="https://twitter.com/MrCippy">twitter</a>, <a href="#">instagram</a>, and <s style="color: #c6c5c2;">facebook</s>.', { delay: 400})
   .go();
