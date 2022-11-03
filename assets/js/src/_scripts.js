e08bee42720e786828187439841f0e6c90cd0ba7
cipriani14 <jason@jasoncipriani.com>
2022-11-03 16:57:31 -0600

    Update _scripts.js

--- assets/js/src/_scripts.js
+++ assets/js/src/_scripts.js
@@ -4,7 +4,7 @@ new TypeIt('#type', {
   .type('I\'m Jason Cipriani')
   .pause(300)
   .options({speed: 45})
-  .type(', a writer for ZDNET, IGN, CNN Underscored, The Arena Group, InsideHook, and Tom\'s Guide.<br><br>', { delay: 400})
+  .type(', a writer for <a href="https://www.zdnet.com/meet-the-team/jason-cipriani/">ZDNET</a>, <a href="https://www.ign.com/person/mrcippy">IGN</a>, <a href="https://www.google.com/search?client=safari&hl=en-us&sxsrf=ALeKk01v3Eno-ashHq1NAQy7ZQbWQA-Fsg%3A1612387165408&ei=XRMbYOCrGPHptQaogYP4Cg&q=site%3Acnn.com+jason+cipriani&oq=site%3Acnn.com+jason+cipriani&gs_lcp=CgZwc3ktYWIQA1CJHVjnJWCGJ2gBcAB4AIABbogB8gKSAQM0LjGYAQCgAQGqAQdnd3Mtd2l6wAEB&sclient=psy-ab&ved=0ahUKEwigzu-h0s7uAhXxdM0KHajAAK8Q4dUDCAw&uact=5">CNN Underscored</a>, <a href="https://www.si.com/showcase/author/jason-cipriani">Sports Illustrated Showcase</a>, <a href="https://www.thestreet.com/author/jason-cipriani">The Street</a>, <a href="https://parade.com/author/jason-cipriani">Parade</a>, <a href="https://www.insidehook.com/author/jasoncipriani">InsideHook</a>, and <a href="https://www.tomsguide.com/author/jason-cipriani">Tom\ s Guide</a> <br><br>', { delay: 400})
   .pause(200)
   .type('Find me on <a href="https://twitter.com/MrCippy">twitter</a>, <a href="https://instagram.com/mrcippy">instagram</a>, and <a href="https://acebook.com/mrcippy">facebook</a>.', { delay: 400})
   .go();
