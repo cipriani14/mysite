bacde502bf73b3a4f59ba6a52555c27dcf39cec9
Justin Peacock <justin@superrad.dev>
2024-04-15 16:59:01 -0400

    added json file to edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
-new TypeIt('#type', {
-  speed: 60
-})
-  .type('Hi! Welcome to Sips &amp; Pies.<br><br>')
-  .pause(300)
-  .options({speed: 45})
-  .type('We\'re opening in spring 2025. Until then, please follow our journey on <a href="https://www.instagram.com/sipsandpies">Instagram</a>, <a href="https://www.facebook.com/sipsandpies">Facebook</a>, and <a href="https://www.tiktok.com/@sipsandpies">TikTok</a>. Or <a href="mailto:jason@sipsandpies.com">email us</a>.  <br><br>', {delay: 400})
-  .go();
+fetch('assets/js/text.json')
+  .then(response => response.json())
+  .then(data => {
+    new TypeIt('#type', {
+      speed: 60
+    })
+      .type(data.heading)
+      .pause(300)
+      .options({speed: 45})
+      .type(data.text, {delay: 400})
+      .go();
+  });
